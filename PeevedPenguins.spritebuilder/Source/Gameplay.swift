180e71feac3130df4a7fe02cd83c15d01d02b6e1
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-08-17 21:56:31 -0300

    added some features

--- PeevedPenguins.spritebuilder/Source/Gameplay.swift
+++ PeevedPenguins.spritebuilder/Source/Gameplay.swift
@@ -17,6 +17,8 @@ class Gameplay: CCNode {
     weak var pullBackNode: CCNode!
     weak var mouseJointNode: CCNode!
     var mouseJoint: CCPhysicsJoint?
+    var currentPenguin: Penguin?
+    var penguinCatapultJoint: CCPhysicsJoint?
     
     // called when CCB file has completed loading
     func didLoadFromCCB() {
@@ -29,12 +31,77 @@ class Gameplay: CCNode {
         mouseJointNode.physicsBody.collisionMask = []
         
         //visualize physics bodies and joints
-        gamePhysicsNode.debugDraw = true
+        //gamePhysicsNode.debugDraw = true
     }
     
     // called on every touch in this scene
     override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
-        launchPenguin()
+        //launchPenguin() old implementation
+        
+        let touchLocation = touch.locationInNode(contentNode)
+        
+        //start catapult dragging when a touch inside of the catapult arm occurs
+        if CGRectContainsPoint(catapultArm.boundingBox(), touchLocation){
+            //move mouseJointNode to the touch position
+            mouseJointNode.position = touchLocation
+            
+            //setup a spring joint between the mouseJointNode and the catapultArm
+            mouseJoint = CCPhysicsJoint.connectedSpringJointWithBodyA(mouseJointNode.physicsBody, bodyB: catapultArm.physicsBody, anchorA: CGPointZero, anchorB: CGPoint(x: 34, y: 138), restLength: 0, stiffness: 3000, damping: 150)
+            
+            //create a penguin from a ccb-file
+            currentPenguin = CCBReader.load("Penguin") as! Penguin?
+            if let currentPenguin = currentPenguin{
+                // initially position it on the scoop. 34,138 is the position in the node space of the catapultArm
+                let penguinPosition = catapultArm.convertToWorldSpace(CGPoint(x: 34, y: 138))
+                // transform the world position to the node space to which the penguin will be added (gamePhysicsNode)
+                currentPenguin.position = gamePhysicsNode.convertToNodeSpace(penguinPosition)
+                // add it to the physics world
+                gamePhysicsNode.addChild(currentPenguin)
+                // we don't want the penguin to rotate in the scoop
+                currentPenguin.physicsBody.allowsRotation = false
+                
+                // create a joint to keep the penguin fixed to the scoop until the catapult is released
+                penguinCatapultJoint = CCPhysicsJoint.connectedPivotJointWithBodyA(currentPenguin.physicsBody, bodyB: catapultArm.physicsBody, anchorA: currentPenguin.anchorPointInPoints)
+            }
+        }
+        
+    }
+    
+    //when a touch moves
+    override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        //whenever touches move, update the position of the mouseJointNode to the touch position
+        let touchLocation = touch.locationInNode(contentNode)
+        mouseJointNode.position = touchLocation
+    }
+    
+    //when a touch ends
+    func releaseCatapult(){
+        if let joint = mouseJoint {
+            //releases the joint and lets the catapult snap back
+            joint.invalidate()
+            mouseJoint = nil
+            
+            //releases the joint and lets the penguin fly
+            penguinCatapultJoint?.invalidate()
+            penguinCatapultJoint = nil
+            
+            //after snapping rotation is fine
+            currentPenguin?.physicsBody.allowsRotation = true
+            
+            //follow the flying penguin
+            let actionFollow = CCActionFollow(target: currentPenguin, worldBoundary: boundingBox())
+            contentNode.runAction(actionFollow)
+        }
+    }
+    
+    override func touchEnded(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        //when touches end, meaning the user releases their finger, release the catapult
+        releaseCatapult()
+    }
+    
+    override func touchCancelled(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        //when tocuhes cancelled, meaning the user drags their finger off the screen or onto smething else, release the catapult
+        releaseCatapult()
     }
     
     func retry(){
